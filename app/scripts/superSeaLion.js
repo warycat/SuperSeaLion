d50224c5eb44cda9011af500b5a03922e6d7c7df
Larry Fantasy <larry.fantasy@gmail.com>
2014-04-16 21:46:33 -0700

    basic input working

--- app/scripts/superSeaLion.js
+++ app/scripts/superSeaLion.js
@@ -13,6 +13,82 @@ var Screen = (function(){
   };
 })();
 
+var Input = (function(){
+  var keys = {};
+  function keydown(event){
+    var keyCode = event.keyCode;
+    switch(keyCode){
+      case 37:
+      case 38:
+      case 39:
+      case 40:
+      case 27:
+      case 13:
+      case 65:
+        event.preventDefault();
+        keys[keyCode] = true;
+        break;
+      default:
+    }
+  }
+
+  function keyup(event){
+    var keyCode = event.keyCode;
+    switch(keyCode){
+      case 37:
+      case 38:
+      case 39:
+      case 40:
+        event.preventDefault();
+        keys[keyCode] = false;
+        break;
+      case 27:
+        event.preventDefault();
+        keys[keyCode] = false;
+        onEsc();
+        break;
+      case 13:
+      case 65:
+        event.preventDefault();
+        keys[keyCode] = false;
+        onDone();
+        break;
+      default:
+    }
+  }
+  function up(){
+    if(keys[38] === true) return true;
+    return false;
+  }
+
+  function down(){
+    if(keys[40] === true) return true;
+    return false;
+  }
+
+  function left(){
+    if(keys[37] === true) return true;
+    return false;
+  }
+
+  function right(){
+    if(keys[39] === true) return true;
+    return false;
+  }
+  function init(){
+    window.addEventListener('keydown', keydown);
+    window.addEventListener('keyup',keyup);
+  }
+  return {
+    init:init
+  , up:up
+  , left:left
+  , down:down
+  , right:right
+  };
+})();
+
+
 var SSL = (function(){
   var width = 100;
   var height = 100;
@@ -84,7 +160,7 @@ var Renderer = (function(){
 
   function animate(){
     if(paused)return;
-    console.log('.');
+    if(Input.up())console.log('.');
     renderer.render(Screen.stage);
     requestAnimFrame(animate);
   }
@@ -95,10 +171,8 @@ var Renderer = (function(){
   };
 })();
 
-// var Input = (function(){
-  
-// })();
 
+Input.init();
 Renderer.play();
 
 // var enemies = [{name:'fish'},{name:'crab'}];
